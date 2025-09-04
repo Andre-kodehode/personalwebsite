9507b9ef2d4c8a8a2c87abf3b3b6e0dc90192ae0
Andrè <AAA03022025@gmail.com>
2025-09-04 12:48:23 +0200

    renamed config to clientConfig

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,8 +1,8 @@
-export const config = {
+export const clientConfig = {
   projectId: "stw6q4ek",
   dataset: "production",
   apiVersion: "2025-09-03",
   useCdn: true,
 };
 
-export default config;
+export default clientConfig;
